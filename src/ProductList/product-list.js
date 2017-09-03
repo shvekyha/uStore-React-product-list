8ce0f9b47819869fec93a86aab55d5dba2bf137b
hadas <hadas@xmpie.com>
2017-09-03 22:42:45 +0300

    Added initial html file, css classes and basic product-list component

---
+++ src/ProductList/product-list.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class ProductList extends React.Component {
+    render() {
+        return (
+            <section className="mainPageArea">
+                <h2>Product list component is up and running!</h2>
+            </section>
+        );
+      }
+}
+
+export default ProductList;
